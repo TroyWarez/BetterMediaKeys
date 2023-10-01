753f2666fe1be5377bff82a73f3993addb336036
Troy <TroyWarez@troysdomain.com>
2023-10-01 10:43:36 -0700

    Changed the KeyboardEvent

--- mediaKeys.js
+++ mediaKeys.js
@@ -9,11 +9,11 @@ function SetMediaKeys()
     let subdomain = host.split('.')[0]
     
 if ("mediaSession" in navigator && list === false && subdomain !== 'music') {
-    navigator.mediaSession.setActionHandler('previoustrack', function() { 
-        document.dispatchEvent(new KeyboardEvent('keydown', {'keyCode': 37, 'which': 37, 'ctrlKey' : true}));// 37 is "ArrowLeft", 39 is "ArrowRight".
-         });
+    navigator.mediaSession.setActionHandler('previoustrack', function() {
+        document.dispatchEvent(new KeyboardEvent('keydown', {'IsTrusted' : true, 'altKey' : true, 'keyCode': 37, 'which': 37, 'altKey' : true, 'ctrlKey' : true}));// 37 is "ArrowLeft", 39 is "ArrowRight".
+        });
     navigator.mediaSession.setActionHandler('nexttrack', function() {
-            document.dispatchEvent(new KeyboardEvent('keydown', {'keyCode': 39, 'which': 39, 'ctrlKey' : true}));// 37 is "ArrowLeft", 39 is "ArrowRight".
+            document.dispatchEvent(new KeyboardEvent('keydown', {'IsTrusted' : true, 'altKey' : true, 'keyCode': 39, 'which': 39, 'altKey' : true, 'ctrlKey' : true}));// 37 is "ArrowLeft", 39 is "ArrowRight".
             });
     }
 }
