88d4d6cc031b3b954c8438278eeaf93cc6547e30
Troy <TroyWarez@troysdomain.com>
2023-10-07 20:42:12 -0700

    BMK now mimics the logic used on the music player.

--- mediaKeys.js
+++ mediaKeys.js
@@ -1,7 +1,7 @@
 const config = { attributes: true, childList: true, subtree: true };
 var ytInitialData;
 var ytChapterData = null;
-var ytSessionVideoData = null;
+//var ytSessionVideoData = null;
 document.addEventListener("yt-navigate-finish", function (event)
 {
     if(("detail" in event)//Cumbersome
@@ -26,6 +26,7 @@ document.addEventListener("yt-navigate-finish", function (event)
 function SetMediaKeys()// Must now be injected into the current video page.
 {
     const currentChapterText = document.getElementsByClassName("ytp-chapter-title-content")[0];
+    const moviePlayer = document.getElementById("movie_player");
     const url = window.location;
     let params = new URLSearchParams(url.search);
     let list = params.has('list');
@@ -63,11 +64,29 @@ function SetMediaKeys()// Must now be injected into the current video page.
                 if( (typeof currentChapterText !== 'undefined') && ("textContent" in currentChapterText) && (currentChapterText.textContent !== ''))
                 {
                    let CurrentChapterIndex = ytChapterData.chapters.findIndex(i => i.chapterRenderer.title.simpleText === currentChapterText.textContent);
-                   if(CurrentChapterIndex !== -1 && (CurrentChapterIndex - 1) >= 0)
+                   if(CurrentChapterIndex !== -1 && CurrentChapterIndex >= 0)
                    {
-                    moviePlayer.seekToChapterWithAnimation((CurrentChapterIndex - 1));
+                    let StartTimeSec = ((ytChapterData.chapters[CurrentChapterIndex].chapterRenderer.timeRangeStartMillis + 5000) / 1000);
+                    let CurrentTime = moviePlayer.getCurrentTime();
+                    if((StartTimeSec <= CurrentTime))
+                    {
+                        moviePlayer.seekToChapterWithAnimation(CurrentChapterIndex);
+
+                    }
+                    else if((StartTimeSec - 5) === 0)//Cleaner
+                    {
+                        moviePlayer.CurrentTime = 0;
+                        moviePlayer.seekTo(0);
+                        return;
+                    }
+                    else
+                    {
+                        CurrentChapterIndex = CurrentChapterIndex - 1;
+                        moviePlayer.seekToChapterWithAnimation(CurrentChapterIndex);
+                    }
+
+                    navigator.mediaSession.metadata.title = ytChapterData.chapters[CurrentChapterIndex].chapterRenderer.title.simpleText;
                    }
-                    //navigator.mediaSession.metadata.title = currentChapterText.textContent;
                 }
 
             }
@@ -83,7 +102,7 @@ function SetMediaKeys()// Must now be injected into the current video page.
                     navigator.mediaSession.metadata.title = currentChapterText.textContent;
                 }
                 const moviePlayer = document.getElementById("movie_player");
-                if((moviePlayer !== null) && ("seekToChapterWithAnimation" in moviePlayer) && (typeof currentChapterText !== 'undefined') && ("textContent" in currentChapterText) && (currentChapterText.textContent !== '')){
+                if((moviePlayer !== null) && ("seekToChapterWithAnimation" in moviePlayer) && ("seekTo" in moviePlayer) && (typeof currentChapterText !== 'undefined') && ("textContent" in currentChapterText) && (currentChapterText.textContent !== '')){
                     //moviePlayer.previousVideo(); doesn't work unless your in a playlist.
                     //history.back();
                     if( (typeof currentChapterText !== 'undefined') && ("textContent" in currentChapterText) && (currentChapterText.textContent !== ''))
@@ -92,7 +111,6 @@ function SetMediaKeys()// Must now be injected into the current video page.
                        if(CurrentChapterIndex !== -1 && (CurrentChapterIndex + 1) >= 0)
                        {
                             moviePlayer.seekToChapterWithAnimation((CurrentChapterIndex + 1));
-                            navigator.mediaSession.metadata.title = currentChapterText.textContent;
                        }
                     }
     
