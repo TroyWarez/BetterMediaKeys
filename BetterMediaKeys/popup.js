46c222513d6c36c6f4aa21cf32356c1357f8790e
Troy <TroyWarez@troysdomain.com>
2025-06-06 14:05:37 -0700

    ui update

--- BetterMediaKeys/popup.js
+++ BetterMediaKeys/popup.js
@@ -1,5 +1,2 @@
 const loop_time = document.querySelector("#loop_time");
 const loop_time_range = document.querySelector("#duration");
-loop_time_range.addEventListener("input", (event) => {
-loop_time.textContent = event.target.value
-});
