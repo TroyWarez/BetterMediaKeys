478b67970c5157c9df79ed654b3eff5fd131d844
Troy <TroyWarez@troysdomain.com>
2025-06-10 15:42:20 -0700

    final gui

--- BetterMediaKeys/main.js
+++ BetterMediaKeys/main.js
@@ -1,3 +1,11 @@
 const script = document.createElement('script');
+const defaultConfig = {
+    LoopVidoes: false,
+    minLoopVideoDuration: 0,
+    swapTitle: false,
+    minSwapTitleVideoDuration: 0,
+    previousTrackCmd: 'RESTART_VIDEO',
+    nextTrackCmd: 'NEXT_VIDEO',
+};
 script.src = chrome.runtime.getURL('mediaKeys.js');
 document.documentElement.appendChild(script);
--- BetterMediaKeys/popup.js
+++ BetterMediaKeys/popup.js
@@ -1,2 +1,13 @@
 const loop_time = document.querySelector("#loop_time");
 const loop_time_range = document.querySelector("#duration");
+
+const loop_time_long = document.querySelector("#loop_timeLong");
+const loop_time_range_long = document.querySelector("#durationLong");
+
+loop_time_range.addEventListener("input", (event) => {
+loop_time.textContent = event.target.value / 60 + ':00';
+});
+
+loop_time_range_long.addEventListener("input", (event) => {
+loop_time_long.textContent = event.target.value / 60 + ':00';
+});
